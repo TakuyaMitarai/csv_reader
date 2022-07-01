5ddfc4451e412e3ba30d65714f4f26b2cdfec8fc
mitarai <aaa>
2022-07-01 12:23:33 +0900

    完成

--- slope.cpp
+++ slope.cpp
@@ -37,43 +37,11 @@ int main()
         //printf("%d, %f\n", i, slope[i]);
     }
 
-    //傾き算出
-    /*for(i = 0; i< GEN_MAX - 1; i++) {
-        slope[i] = ave[i+1] - ave[i];
-        printf("%d, %f\n", i, slope[i]);
-    }*/
-
-    //0の個数算出
+    //平均値がある定数を下回るまでカウントし、出力
     for(i = 0; i< GEN_MAX - 1; i++) {
         if(slope[i] > 0.03){
             count_0++;
         }
     }
     printf("GENは%d\n", count_0);
-
-    //区切り箇所表示
-    /*
-    count_0 = 0; //count_0の初期化
-    for(i = 0; i< GEN_MAX - 1; i++) {
-        if(slope[i] == 0){
-            count_0++;
-        }
-        if(count_0 == DELIMITER){
-            printf("DELIMITERが%dのときEXECUTION_TIMESが%d\n", DELIMITER, i);
-            break;
-        }
-    }
-    */
-
-   //区切りを10刻みで変えた時の変化を求める
-    j = 0;
-    count_0_1 = 0;
-    for(i = 0; i< GEN_MAX - 1; i++) {
-        if(slope[i] > 0.03){
-            count_0_1++;
-        }else{
-            printf("%d\n", i);
-            break;
-        }
-    }
 }
